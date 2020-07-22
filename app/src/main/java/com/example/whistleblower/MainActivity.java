23535c94dde533e6eb8e4aad142af684c5014031
Devanshi Udeshi <devanshiudeshi@fb.com>
2020-07-22 13:30:57 -0700

    Cleaned up the code

--- app/src/main/java/com/example/whistleblower/MainActivity.java
+++ app/src/main/java/com/example/whistleblower/MainActivity.java
@@ -1,54 +1,19 @@
 package com.example.whistleblower;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.example.whistleblower.api.ApiClient;
-import com.example.whistleblower.api.ApiInterface;
 import com.example.whistleblower.fragments.ComposeFragment;
 import com.example.whistleblower.fragments.NewsFragment;
 import com.example.whistleblower.fragments.PostsFragment;
 import com.example.whistleblower.fragments.ProfileFragment;
-import com.example.whistleblower.models.News;
-import com.example.whistleblower.models.NewsArticle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,24 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
-//    private RecyclerView recyclerView;
-//    private RecyclerView.LayoutManager layoutManager;
-//    private List<NewsArticle> articles = new ArrayList<>();
-//    private NewsAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        recyclerView = findViewById(R.id.rvMain);
-//        layoutManager = new LinearLayoutManager(MainActivity.this);
-//        recyclerView.setLayoutManager(layoutManager);
-//        recyclerView.setItemAnimator(new DefaultItemAnimator());
-//        recyclerView.setNestedScrollingEnabled(false);
-//
-//        LoadJson();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         // Using switch statements to see which bottom menu item was selected, create fragment, and then replace
@@ -109,36 +62,4 @@ public class MainActivity extends AppCompatActivity {
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
-
-//    public void LoadJson() {
-//        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-//
-//        String country = Utilities.getCountry();
-//
-//        Call<News> call;
-//        call = apiInterface.getNews(country, API_KEY);
-//
-//        call.enqueue(new Callback<News>() {
-//            @Override
-//            public void onResponse(Call<News> call, Response<News> response) {
-//                if (response.isSuccessful() && response.body().getArticle() != null){
-//                    if (!articles.isEmpty()) {
-//                        articles.clear();
-//                    }
-//
-//                    articles = response.body().getArticle();
-//                    adapter = new NewsAdapter(articles, MainActivity.this);
-//                    recyclerView.setAdapter(adapter);
-//                    adapter.notifyDataSetChanged();
-//                } else {
-//                    Toast.makeText(MainActivity.this, "No Result", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<News> call, Throwable t) {
-//
-//            }
-//        });
-//    }
 }
